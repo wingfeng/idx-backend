bb738aedaac0cac6e0f3534bbd2094eab186078a
wingfeng <wingfeng@gmail.com>
2024-09-25 22:03:41 +0800

    add mime tpe for mjs file

---
+++ front/apps/web-antd/src/views/_core/authentication/login.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import { UserManager } from 'oidc-client-ts';
+
+import { oidcSetting } from '#/config/oidcSetting';
+
+export default defineComponent({
+  name: 'Login',
+  mounted() {
+    const userManager = new UserManager(oidcSetting);
+    setTimeout(() => {
+      userManager.signinRedirect();
+    }, 500);
+  },
+});
+</script>
+
+<template>
+  <div>
+    <h1>IDX Login</h1>
+  </div>
+</template>
